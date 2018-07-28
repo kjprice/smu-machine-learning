1cc2bc7abc7c3d7f57160504a2d8350132e901c2
KJ <kjprice12@gmail.com>
2018-07-28 14:48:53 -0500

    Throttle requests

--- get-posters/index.js
+++ get-posters/index.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 const fs = require('fs');
 const assert = require('assert');
-const request = require('superagent');
+const request = require('request');
+const throttledRequest = require('throttled-request')(request);
+
+// we are limitted 40 requests every ten seconds
+const LIMIT_RATE = 30;
+const TEN_SECONDS = 10 * 1000;
+throttledRequest.configure({
+  requests: LIMIT_RATE,
+  milliseconds: TEN_SECONDS,
+});
+
 
 const apiKey = process.env.TMDB_API_KEY;
 const tmdbEndpoint = 'https://api.themoviedb.org/3/movie';
@@ -15,26 +25,31 @@ function retrieveIdsFromCsv() {
 }
 
 function retrievePosterPath(movieId) {
-  const movieEndpoint = `${tmdbEndpoint}/${movieId}`;
-  console.log(movieEndpoint);
-  return request
-    .get(movieEndpoint)
-    .query({ api_key: apiKey })
-    .set('accept', 'json')
-    .then((movieInfo) => {
-      const { poster_path: posterPath } = movieInfo.body;
-      return posterPath;
+  return new Promise((res, rej) => {
+    const movieEndpoint = `${tmdbEndpoint}/${movieId}?api_key=${apiKey}`;
+    return throttledRequest(movieEndpoint, (error, response, body) => {
+      const { poster_path: posterPath } = JSON.parse(body);
+      res(posterPath);
     });
+  })
+}
+
+function getPosterForMovieWithThrottle(movieId) {
+  return retrievePosterPath(movieId)
+  .then((posterPath) => {
+    console.log(posterPath);
+    return posterPath;
+  });
 }
 
 function main() {
   const movieIds = retrieveIdsFromCsv();
   assert.equal(movieIds.length, 4803);
   
-  return retrievePosterPath(movieIds.shift())
-  .then((posterPath) => {
-    console.log(posterPath);
-  });
+  movieIds.slice(0, 42).forEach((movieId) => (
+    getPosterForMovieWithThrottle(movieId)
+    .then(() => console.log('all done'))
+  ));
 }
 
 main();
