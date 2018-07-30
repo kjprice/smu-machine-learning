6622212f4b73a31764f58ab7a695beb214cc440b
KJ <kjprice12@gmail.com>
2018-07-30 09:55:55 -0500

    Pipe samuel images to local

--- get-samuel-images/index.js
+++ get-samuel-images/index.js
@@ -4,13 +4,36 @@ const throttledRequest = require('throttled-request')(request);
 
 const IMG_URLS_FILEPATH = 'data/samuel-images-urls.txt';
 
+const LIMIT_RATE = 26;
+const TEN_SECONDS = 10 * 1000;
+throttledRequest.configure({
+  requests: LIMIT_RATE,
+  milliseconds: TEN_SECONDS,
+});
+
 function getUrls() {
   return fs.readFileSync(IMG_URLS_FILEPATH, { encoding: 'utf8'})
   .split('\n');
 }
 
+function pipeImageToLocal(url, imageNumber) {
+  return new Promise((res, rej) => {
+    return throttledRequest(url)
+    .pipe(fs.createWriteStream(`data/images/samuel/${imageNumber}.jpg`))
+    .on('error', (e) => {
+      rej(e);
+    });
+  });
+}
+
+function downloadImagesFromUrls(urls) {
+  return Promise.all(urls.map(pipeImageToLocal))
+}
+
 function main() {
   const urls = getUrls();
+  downloadImagesFromUrls(urls)
+  .then(() => {console.log('all done')});
 }
 
 main();
